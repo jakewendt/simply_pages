b69c2952373ff28f7948237deaa11fb786e37e9a
Jake <github@jake.otherinbox.com>
2010-10-28 10:22:27 -0700

    Version bump to 1.0.5

---
+++ lib/simply_pages/test_tasks.rb
@@ -0,0 +1,23 @@
+module SimplyPages;end
+namespace :test do
+	namespace :units do
+		Rake::TestTask.new(:simply_pages => "db:test:prepare") do |t|
+			t.pattern = File.expand_path(File.join(
+				File.dirname(__FILE__),'/../../test/unit/**/*_test.rb'))
+			t.libs << "test"
+			t.verbose = true
+		end
+	end
+	namespace :functionals do
+		Rake::TestTask.new(:simply_pages => "db:test:prepare") do |t|
+			t.pattern = File.expand_path(File.join(
+				File.dirname(__FILE__),'/../../test/functional/**/*_test.rb'))
+			t.libs << "test"
+			t.verbose = true
+		end
+	end
+end
+Rake::Task['test:functionals'].prerequisites.unshift(
+	"test:functionals:simply_pages" )
+Rake::Task['test:units'].prerequisites.unshift(
+	"test:units:simply_pages" )
