b599a158912293e75f423dae80966c0712efaa73
Jake <github@jakewendt.com>
2011-05-26 16:17:37 -0700

    Added gem_test_dirs so that gem tests are run in rake test:coverage

--- lib/simply_pages/test_tasks.rb
+++ lib/simply_pages/test_tasks.rb
@@ -26,4 +26,13 @@ Rake::Task['test:units'].prerequisites.unshift(
 #	but that would make __FILE__ different.
 #	Hmmm
 
+#
+#	used in simply_helpful's rake test:coverage to run gem's 
+#		tests in the context of the application
+#
+@gem_test_dirs ||= []
+@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
+	'/../../test/unit/pages/'))
+@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
+	'/../../test/functional/pages/'))
 
